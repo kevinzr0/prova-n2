f8e237e59847ac2250f1c7e07baf05563484dc14
Kevin <kevinzorzan7@gmail.com>
2023-11-29 20:09:47 -0300

    Mensagem de commit

---
+++ src/test/java/br/com/unidep/demoprovan2/DemoProvaN2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unidep.demoprovan2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProvaN2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
